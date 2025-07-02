937e3e062d6d391aaf097e91b7d205faa709e67d
Scott Aaron <dr_saaron@yahoo.com>
2025-07-02 16:01:07 -0500

    version update

--- src/main/java/com/blazartech/springbatchrepoexplorer/ListJobExecutionsCommandLineRunner.java
+++ src/main/java/com/blazartech/springbatchrepoexplorer/ListJobExecutionsCommandLineRunner.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Slf4j
-public class ListJobExecutionsCommandLIneRunner implements CommandLineRunner {
+public class ListJobExecutionsCommandLineRunner implements CommandLineRunner {
 
     @Autowired
     private JobExplorer jobExplorer;
